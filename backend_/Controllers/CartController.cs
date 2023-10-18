908ebccfb5ceedac35d79e1ab5308f814b6f0c10
Josue B Centurion <josuebcenturion@gmail.com>
2023-10-18 20:19:20 -0300

    GENERAL AUTH0 ADDED.

--- backend_/Controllers/CartController.cs
+++ backend_/Controllers/CartController.cs
@@ -2,17 +2,18 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using static backend_.Models.Users;
+using static backend_.Models.Carts;
 
 namespace backend_.Controllers
 {
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : Controller
+    public class CartController : ControllerBase
     {
         private readonly IDynamoDBContext _context;
 
-        public UsersController(IDynamoDBContext context)
+        public CartController(IDynamoDBContext context)
         {
             _context = context;
         }
@@ -20,23 +21,22 @@ namespace backend_.Controllers
         [HttpGet("{id}/{barcode}")]
         public async Task<IActionResult> Get(string id, string barcode)
         {
-            var product = await _context.LoadAsync<User>(id, barcode);
+            var product = await _context.LoadAsync<Cart>(id, barcode);
             if (product == null) return NotFound();
             return Ok(product);
         }
 
         [HttpGet]
-        // [Authorize("read:admin-messages")]
         public async Task<IActionResult> GetAll()
         {
-            var products = await _context.ScanAsync<User>(default).GetRemainingAsync();
+            var products = await _context.ScanAsync<Cart>(default).GetRemainingAsync();
             return Ok(products);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(User request)
+        public async Task<IActionResult> Create(Cart request)
         {
-            var product = await _context.LoadAsync<User>(request.Id, request.TypeOfUser);
+            var product = await _context.LoadAsync<Cart>(request.Id, request.TypeOfUser);
             if (product != null) return BadRequest($"Product with Id {request.Id} and BarCode {request.TypeOfUser} Already Exists");
             await _context.SaveAsync(request);
             return Ok(request);
@@ -45,16 +45,16 @@ namespace backend_.Controllers
         [HttpDelete("{id}/{barcode}")]
         public async Task<IActionResult> Delete(string id, string barcode)
         {
-            var product = await _context.LoadAsync<User>(id, barcode);
+            var product = await _context.LoadAsync<Cart>(id, barcode);
             if (product == null) return NotFound();
             await _context.DeleteAsync(product);
             return NoContent();
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update(User request)
+        public async Task<IActionResult> Update(Cart request)
         {
-            var product = await _context.LoadAsync<User>(request.Id, request.TypeOfUser);
+            var product = await _context.LoadAsync<Cart>(request.Id, request.TypeOfUser);
             if (product == null) return NotFound();
             await _context.SaveAsync(request);
             return Ok(request);
--- backend_/Program.cs
+++ backend_/Program.cs
@@ -1,6 +1,12 @@
 using Amazon.S3;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Auth0.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +22,27 @@ builder.Services.AddDefaultAWSOptions(awsOptions);
 builder.Services.AddAWSService<IAmazonDynamoDB>();
 builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
 
+// Ading Auth0
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = "https://dev-x3wpn6igqgxpdpqa.us.auth0.com/";
+    options.Audience = "https://commerce-it";
+});
+
+builder.Services.AddCors(); 
+
 var app = builder.Build();
+
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -27,8 +53,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
+
+app.UseAuthorization();
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
